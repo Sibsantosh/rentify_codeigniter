ddceddef42a11d8816652e88571dd13380fad2d6
Sibsantosh <sibsantosh37@gmail.com>
2024-10-17 17:18:29 +0530

    added modular top and side navigation

--- app/Views/SplashScreen.php
+++ app/Views/SplashScreen.php
@@ -32,36 +32,3 @@
 
 </html>
 
-
-<?php
-
-
-$ch = curl_init();
-$url = "https://172.16.8.153/fmi/data/vLatest/databases/Rentify/sessions";
-$username = 'admin';
-$password = 'Sibsantosh@2580';
-$dataArray = array("{}");
-$data = json_encode(new stdClass());
-
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST,1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
-    'Authorization: Basic ' . base64_encode("$username:$password")));
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-$resp = curl_exec($ch);
-$token = "";
-if($e= curl_error($ch)){
-    var_dump( "error". $e);
-}else{
-    $decodedJson = json_decode($resp, true);
-    $token = $decodedJson["response"]["token"];
-    setcookie("auth_token", $token, time() + 9*60000,"/");
-    var_dump("". $token ."");
-
-    
-}
-
-?>
